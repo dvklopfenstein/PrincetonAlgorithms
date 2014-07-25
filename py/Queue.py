fbccc7ba8847f6392a4d0085e7b515ad41c6f386
Debra Klopfenstein <DebraKlopfenstein@users.noreply.github.com>
2014-07-25 10:22:28 -0400

    Starting to splits tests off into their own directory

--- py/Queue.py
+++ py/Queue.py
@@ -45,7 +45,7 @@
 # case running times for enqueue and dequeue? (04:28)
 # ANSWER: linear time for enqueue and constant time for dequeue.
 
-import unittest
+#import unittest
 
 class Queue:
 
@@ -153,52 +153,53 @@ def run(line):
   sys.stdout.write('(%d left in Queue) OUTPUT: %s\n'%(O.size(),' '.join(res)))
   return res
 
-class Stack_Tests(unittest.TestCase):
-
-  def chk(self, a, txt):
-    b = txt.split()
-    return len(a)==len(b) and len(a)==sum([1 for i,j in zip(a,b) if i==j])
-
-  def test_wk2_ex_Queues_608030(self):
-    # Question 2
-    # (seed = 608030)
-    # Suppose that an intermixed sequence of 10 enqueue and 10
-    # dequeue operations are performed on a FIFO queue. The
-    # enqueues add the letters 0 through 9 in order; the dequeues
-    # print out the return value. Which of the following output
-    # sequence(s) could occur?
-    # r = run("0 1 2 3 4 5 6 7 8 9")
-    print "608030 DONE"
-    r = run("0 - 1 2 - 3 4 5 6 7 8 9")
-    #self.failUnless( self.chk(r, "0 2 1 3 7 4 9 6 8 5") )
-
-    r = run("0 1 2 - - - 3 4 5 - 6 - - 7 8 9")
-    #self.failUnless( self.chk(r, "0 1 2 5 6 4 8 9 3 7") )
-
-    r = run("0 1 2 3 4 5 6 7 8 9 - - - - - - - - - -")
-    self.failUnless( self.chk(r, "0 1 2 3 4 5 6 7 8 9") )
-
-    r = run("0 - 1 2 3 4 5 6 7 8 9")
-    #self.failUnless( self.chk(r, "0 7 4 1 8 3 6 2 9 5") )
-
-    r = run("0 - 1 2 3 4 5 6 7 8 9")
-    #self.failUnless( self.chk(r, "0 4 5 1 8 6 2 3 9 7") )
-
-  def test_wk2_ex_Queues_511394(self):
-    # (seed = 511394)
-    # Suppose that an intermixed sequence of 10 enqueue and 10
-    # dequeue operations are performed on a FIFO queue. The
-    # enqueues add the letters 0 through 9 in order; the dequeues
-    # print out the return value. Which of the following output
-    # sequence(s) could occur?
-    r = run("0 1 - - 2 3 4 - 5 6 7 8 9")
-    #self.failUnless( self.chk(r, "0 1 4 9 6 8 5 2 7 3") )
-    #self.failUnless( self.chk(r, "0 1 2 3 4 5 8 9 6 7") )
-    #self.failUnless( self.chk(r, "0 1 2 4 8 6 5 9 7 3") )
-    #self.failUnless( self.chk(r, "0 1 2 3 4 7 6 8 5 9") )
-    #self.failUnless( self.chk(r, "0 1 2 3 4 5 6 7 8 9") )
+#class Stack_Tests(unittest.TestCase):
+#
+#  def chk(self, a, txt):
+#    b = txt.split()
+#    return len(a)==len(b) and len(a)==sum([1 for i,j in zip(a,b) if i==j])
+#
+#  def test_wk2_ex_Queues_608030(self):
+#    # Question 2
+#    # (seed = 608030)
+#    # Suppose that an intermixed sequence of 10 enqueue and 10
+#    # dequeue operations are performed on a FIFO queue. The
+#    # enqueues add the letters 0 through 9 in order; the dequeues
+#    # print out the return value. Which of the following output
+#    # sequence(s) could occur?
+#    # r = run("0 1 2 3 4 5 6 7 8 9")
+#    print "608030 DONE"
+#    r = run("0 - 1 2 - 3 4 5 6 7 8 9")
+#    #self.failUnless( self.chk(r, "0 2 1 3 7 4 9 6 8 5") )
+#
+#    r = run("0 1 2 - - - 3 4 5 - 6 - - 7 8 9")
+#    #self.failUnless( self.chk(r, "0 1 2 5 6 4 8 9 3 7") )
+#
+#    r = run("0 1 2 3 4 5 6 7 8 9 - - - - - - - - - -")
+#    self.failUnless( self.chk(r, "0 1 2 3 4 5 6 7 8 9") )
+#
+#    r = run("0 - 1 2 3 4 5 6 7 8 9")
+#    #self.failUnless( self.chk(r, "0 7 4 1 8 3 6 2 9 5") )
+#
+#    r = run("0 - 1 2 3 4 5 6 7 8 9")
+#    #self.failUnless( self.chk(r, "0 4 5 1 8 6 2 3 9 7") )
+#
+#  def test_wk2_ex_Queues_511394(self):
+#    # (seed = 511394)
+#    # Suppose that an intermixed sequence of 10 enqueue and 10
+#    # dequeue operations are performed on a FIFO queue. The
+#    # enqueues add the letters 0 through 9 in order; the dequeues
+#    # print out the return value. Which of the following output
+#    # sequence(s) could occur?
+#    r = run("0 1 - - 2 3 4 - 5 6 7 8 9")
+#    #self.failUnless( self.chk(r, "0 1 4 9 6 8 5 2 7 3") )
+#    #self.failUnless( self.chk(r, "0 1 2 3 4 5 8 9 6 7") )
+#    #self.failUnless( self.chk(r, "0 1 2 4 8 6 5 9 7 3") )
+#    #self.failUnless( self.chk(r, "0 1 2 3 4 7 6 8 5 9") )
+#    #self.failUnless( self.chk(r, "0 1 2 3 4 5 6 7 8 9") )
 
 if __name__ == '__main__':
-  unittest.main()
+  #unittest.main()
+  run("to be or not to be - - - - - -")
 
 
