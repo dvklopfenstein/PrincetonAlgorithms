4f4882885345e4616b760eefe224ef4ad443bd86
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2015-09-14 07:30:58 -0400

    Moved code to top of file.  comments below.

--- py/AlgsSedgewickWayne/Queue.py
+++ py/AlgsSedgewickWayne/Queue.py
@@ -1,4 +1,68 @@
 """Class Queue."""
+s
+
+class Queue(object):
+  """Queue container class."""
+
+  class _Node(object):
+    """Queue will contain _Nodes."""
+    def __init__(self, item, last):
+      self.Item = item
+      self.Next = last
+
+  def __init__(self):
+    self.N = 0        # number of elements on queue
+    self.first = None # Pointer to begining of queue
+    self.last = None  # Pointer to end of queue
+
+  def isEmpty(self):
+    """True if Queue is empty."""
+    return self.first == None
+
+  def enqueue(self, item):  # Week 2, "Queues" 02:32
+    """Add new items to the back of the queue."""
+    oldlast = self.last     # Save a link to last Node
+    self.last = self._Node(item, None)  # Create a new Node at end of list
+    # Link the new node to the end of the list
+    if self.isEmpty():
+      self.first = self.last
+    else:
+      oldlast.Next = self.last
+    self.N += 1
+
+  def dequeue(self):
+    """Remove old items from the front of the list."""
+    if self.isEmpty():
+      raise Exception("Queue underflow")
+    item = self.first.Item
+    self.first = self.first.Next
+    self.N -= 1
+    if self.isEmpty():
+      self.last = None  # to avoid loitering
+    return item
+
+  def size(self):
+    """Return size of Queue."""
+    return self.N
+
+  def peek(self):
+    """Returns the item least recently added to this queue."""
+    if self.isEmpty():
+      raise Exception("Queue underflow")
+    return self.first.Item
+
+  def __str__(self):
+    """Returns a string representation of this queue."""
+    return ' '.join([str(item) for item in self])
+
+  def __iter__(self):
+    """Is an iterator that returns (yeilds) each item in this queue in FIFO order."""
+    curr_node = self.first
+    while curr_node is not None:
+      item = curr_node.Item
+      curr_node = curr_node.Next
+      yield item
+    raise StopIteration
 
 # https:class.coursera.org/algs4partI-005/lecture/20
 # Week 2, Algorithms 1
@@ -46,67 +110,3 @@
 # recently added ( the end of the list). What are the worst
 # case running times for enqueue and dequeue? (04:28)
 # ANSWER: linear time for enqueue and constant time for dequeue.
-
-class Queue(object):
-  """Queue container class."""
-
-  class _Node(object):
-    """Queue will contain _Nodes."""
-    def __init__(self, item, last):
-      self.Item = item
-      self.Next = last
-
-  def __init__(self):
-    self.N = 0        # number of elements on queue
-    self.first = None # Pointer to begining of queue
-    self.last = None  # Pointer to end of queue
-
-  def isEmpty(self):
-    """True if Queue is empty."""
-    return self.first == None
-
-  def size(self):
-    """Return size of Queue."""
-    return self.N
-
-  def enqueue(self, item):  # Week 2, "Queues" 02:32
-    """Add new items to the back of the queue."""
-    oldlast = self.last     # Save a link to last Node
-    self.last = self._Node(item, None)  # Create a new Node at end of list
-    # Link the new node to the end of the list
-    if self.isEmpty():
-      self.first = self.last
-    else:
-      oldlast.Next = self.last
-    self.N += 1
-
-  def dequeue(self):
-    """Remove old items from the front of the list."""
-    if self.isEmpty():
-      raise Exception("Queue underflow")
-    item = self.first.Item
-    self.first = self.first.Next
-    self.N -= 1
-    if self.isEmpty():
-      self.last = None  # to avoid loitering
-    return item
-
-  def peek(self):
-    """Returns the item least recently added to this queue."""
-    if self.isEmpty():
-      raise Exception("Queue underflow")
-    return self.first.Item
-
-  def __str__(self):
-    """Returns a string representation of this queue."""
-    return ' '.join([str(item) for item in self])
-
-  def __iter__(self):
-    """Is an iterator that returns (yeilds) each item in this queue in FIFO order."""
-    curr_node = self.first
-    while curr_node is not None:
-      item = curr_node.Item
-      curr_node = curr_node.Next
-      yield item
-    raise StopIteration
-
