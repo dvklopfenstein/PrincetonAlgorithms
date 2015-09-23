612a1a0f0d2092b5a30f9de0ff04d40289a400ab
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2015-09-23 06:05:28 -0400

    Added more comments from lecture.

--- py/AlgsSedgewickWayne/Quick3way.py
+++ py/AlgsSedgewickWayne/Quick3way.py
@@ -78,5 +78,49 @@ def _isSorted(a, lo=None, hi=None):
 #   NO  items in strictly increasing order
 #   NO  items in strictly decreasing orer
 
+#######################################################################
+# System Sorts (11:50) Algs 1, Week 3
+#######################################################################
+# Sorting algorithms are essential in a broad variety of applications:
+#   Obvious Applications:
+#     * Sort a list of names.
+#     * Organize a MP3 library.
+#     * Display Google PageRank results.
+#     * List RSS feed in reverse chronological order.
+#   Problems become easy once items are sorted
+#     * Find the median
+#     * Binary search in a database
+#     * Identify statistical outliers.
+#     * Find duplicates in a mailing list.
+#   Non-obvious applications
+#     * Data compression.
+#     * Computer graphics.
+#     * Computational biology.
+#     * Load balancing on a parallel computer.
+
+# QUESTION: Why does Array.sort() in Java use mergesort instead of 
+# quicksort when sorting reference types?
+#   YES stability
+#       N log N guaranteed perfoormance
+#   YES both A and B
+#       neither A and B
+
+# WAR STORY (C QSORT FUNCTION)
+# AT&T Bell Labs (1991). Allan Wilks and Rick Becker discovered that a
+# qsort() call that should have taken a few minutes was consuming 
+# hours of CPU time.
+#
+# At the time, almost all qsort() implementations based on those in:
+# * Version 7 Unix (1979): quadratic time to sort organ-pipe arrays.
+# * BSD Unix (1983): quadratic time to sort random arrays of 0s and 1s.
+
+# Selection   X   N^2/2     N^2/2    N^2/2  N exchanges
+# Insertion   X X N^2/2     N^2/4        N  use for small N or partially ordered
+# Shell       X       ?         ?        N  tight code, subquadratic
+# Merge         X N lg N   N lg N   N lg N  N lg N guarantee, stable
+# Quick       X   N^2/2   2N ln N   N lg N  N lg N probabilistic guarantee, fastest in practice
+# 3-way quick X   N^2/2   2N ln N        N  better w/duplicate keys
+# ???         X X N lg N   N lg N   N lg N  holy sorting grail
+
 # Copyright (C) 2002-2010, Robert Sedgewick and Kevin Wayne.
 # Last updated: Tue Sep 24 11:52:34 EDT 2013.
