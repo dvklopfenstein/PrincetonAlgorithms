6a451d3719f6ee81b9fa5dd1778a3e29db05d16b
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2015-09-25 22:28:56 -0400

    Downloaded latest MaxPQ.java from website.  Moved notes into comments in MaxPQ.py

--- thirdparty/MaxPQ.java
+++ thirdparty/MaxPQ.java
@@ -1,185 +1,317 @@
-# Algorithms, Part 1 from Princeton University
-# by Kevin Wayne, Robert Sedgewick
-# https://class.coursera.org/algs4partI-005 retreived July 2014
-# Lecture 8 - 2 Binary Heaps (23-36)
+/******************************************************************************
+ *  Compilation:  javac MaxPQ.java
+ *  Execution:    java MaxPQ < input.txt
+ *  Dependencies: StdIn.java StdOut.java
+ *  
+ *  Generic max priority queue implementation with a binary heap.
+ *  Can be used with a comparator instead of the natural order,
+ *  but the generic Key type must still be Comparable.
+ *
+ *  % java MaxPQ < tinyPQ.txt 
+ *  Q X P (6 left on pq)
+ *
+ *  We use a one-based array to simplify parent and child calculations.
+ *
+ *  Can be optimized by replacing full exchanges with half exchanges 
+ *  (ala insertion sort).
+ *
+ ******************************************************************************/
 
-# BINARY   TREE: Empty or node with links to left and right binary trees.
-# COMPLETE TREE: Pefectly balanced, except for bottom level.
+package edu.princeton.cs.algs4;
 
-PROPERTY: Height of complete tree with N nodes is [lg N]
-PROOF: Height only increases whe N is a pwer of 2
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-BINARY HEAP: Array representation of a heap-ordered complete binary tree.
+/**
+ *  The <tt>MaxPQ</tt> class represents a priority queue of generic keys.
+ *  It supports the usual <em>insert</em> and <em>delete-the-maximum</em>
+ *  operations, along with methods for peeking at the maximum key,
+ *  testing if the priority queue is empty, and iterating through
+ *  the keys.
+ *  <p>
+ *  This implementation uses a binary heap.
+ *  The <em>insert</em> and <em>delete-the-maximum</em> operations take
+ *  logarithmic amortized time.
+ *  The <em>max</em>, <em>size</em>, and <em>is-empty</em> operations take constant time.
+ *  Construction takes time proportional to the specified capacity or the number of
+ *  items used to initialize the data structure.
+ *  <p>
+ *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/24pq">Section 2.4</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ *
+ *  @param <Key> the generic type of key on this priority queue
+ */
 
-HEAP-ORDERED BINARY TREE: (01:44)
-* Keys in nodes (associate information with each node)
-* Parent's key is no smaller than children's keys
+public class MaxPQ<Key> implements Iterable<Key> {
+    private Key[] pq;                    // store items at indices 1 to N
+    private int N;                       // number of items on priority queue
+    private Comparator<Key> comparator;  // optional Comparator
 
-ARRAY REPRESENTATION: (02:06)
-* Indices start at 1
-* Take nodes in **level** order
-* No explicit links needed!
-
-
-# COMPLETE TREE WITH N=16 NODES (HEIGHT = 4) 00:54
-
-            ______T______
-           /             \
-        __S__           __R__
-       /     \         /     \
-      P       N       O       A
-     / \     / \     / \     / \
-    E   I   H   G   *   *   *   *
-   / \ / \ / \ / \ / \ / \ / \ / \ 
-  *
- / \
-
-i     0  1  2  3  4  5  6  7  8  9 10 11
-a[i]  -  T  S  R  P  N  O  A  E  I  H  G 
-a[i]  -  T                              
-            S  R                        
-            |  +---------+
-            +--------    --
-                  /  \  /  \
-                  P  N  O  A            
-                  |  +---------------+
-                  +--------------    --
-                              /  \  /  \
-                              E  I  H  G
-
-PROPOSITION: Largest key is a[1], which is root of binary tree
-
-PROPOSITION: Can use array indices to move through tree. (03:19)
-* Parent of node at k is at k/2
-* Children of node at k are at 2k and 2k+1
-
-PROMOTION IN A HEAP: (05:15)
-Scenario: Child's key becomes larger key than its parents key.
-To eliminate the violation:
-* Exchange key in child with key in parent
-* Repeat until heap order restored.
-Peter principle: Node promoted to level of incompetence.
-
-INSERTION IN A HEAP:
-Insert: Add node at end, then swim it up.
-Cost: At most 1+lg(N) compares
-
-
-DEMOTION IN A HEAP (07:47):
-Scenario: Parent's key becomes smaller than one (or both of its children)
-To eliminate the violation:
-* Exchange key in parent with key in **larger** child.
-* Repeat until heap order restored.
-Power struggle: Better subordinate promoted
-
-DELETE THE MAXIMUM IN A HEAP (10:03)
-Delete max: Exchange root(max) with node at end, then sink it down.
-Cost: At most 2*lg(N) compares
-
-
-# Binary Heap Implementation
-# * Very Simple
-# * Optimal representation of data
-# * Just a little arithmetic
-public class MaxPQ<Key extends Comparable<Key>> # 15:01
-{
-  private Key[] pq;
-  private int N;
-
-  public MaxPQ(int capacity)
-  { pq = (Key[]) new Comparable[capacity+1]; }
-
-  /// PQ ops
-  public boolean isEmpty()
-  { return N == 0; }
-  public void insert(Key x) // 06:27
-  {
-    pq[++N] = x;
-    swim(N);
-  }
-  public Key delMax() // 10:03
-  {
-    Key max = pq[1];
-    # Exchange root(max) with node at end, 
-    exch(1, N--);
-    # then sink new root down to where it belongs.
-    sink(1);
-    # Prevent loitering by nulling out max position
-    pq[N+1] = null;
-    return max;
-  }
-  
-  /// heap helper functions 
-  private void swim(int k) // 05:15
-  {
-    while(k>1 && less(k/2, k))
-    {
-      exch(k, k/2);
-      k = k/2;
+    /**
+     * Initializes an empty priority queue with the given initial capacity.
+     *
+     * @param  initCapacity the initial capacity of this priority queue
+     */
+    public MaxPQ(int initCapacity) {
+        pq = (Key[]) new Object[initCapacity + 1];
+        N = 0;
     }
-  }
-  private void sink(int k) // 8:52
-  {
-    while (2*k <= N)
-    {
-      int j = 2*k;
-      # Check if we are going off the end of the heap and which child is larger
-      if (j < N && less(j, j+1)) j++;
-      # If k is not less than either child, then we are done
-      if (!less(k,j)) break;
-      # If k is larger than a child, exchange
-      exch(k,j);
-      k = j;
+
+    /**
+     * Initializes an empty priority queue.
+     */
+    public MaxPQ() {
+        this(1);
     }
-  }
-  
-  /// array helper functions 
-  private boolean less(int i, int j)
-  { return pq[i].compareTo(pq[j]) < 0; }
-  private void exch(int i, int j)
-  { Key t = pq[i]; pq[i] = pq[j]; pq[j] = t; }
+
+    /**
+     * Initializes an empty priority queue with the given initial capacity,
+     * using the given comparator.
+     *
+     * @param  initCapacity the initial capacity of this priority queue
+     * @param  comparator the order in which to compare the keys
+     */
+    public MaxPQ(int initCapacity, Comparator<Key> comparator) {
+        this.comparator = comparator;
+        pq = (Key[]) new Object[initCapacity + 1];
+        N = 0;
+    }
+
+    /**
+     * Initializes an empty priority queue using the given comparator.
+     *
+     * @param  comparator the order in which to compare the keys
+     */
+    public MaxPQ(Comparator<Key> comparator) {
+        this(1, comparator);
+    }
+
+    /**
+     * Initializes a priority queue from the array of keys.
+     * Takes time proportional to the number of keys, using sink-based heap construction.
+     *
+     * @param  keys the array of keys
+     */
+    public MaxPQ(Key[] keys) {
+        N = keys.length;
+        pq = (Key[]) new Object[keys.length + 1]; 
+        for (int i = 0; i < N; i++)
+            pq[i+1] = keys[i];
+        for (int k = N/2; k >= 1; k--)
+            sink(k);
+        assert isMaxHeap();
+    }
+      
+
+
+    /**
+     * Returns true if this priority queue is empty.
+     *
+     * @return <tt>true</tt> if this priority queue is empty;
+     *         <tt>false</tt> otherwise
+     */
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    /**
+     * Returns the number of keys on this priority queue.
+     *
+     * @return the number of keys on this priority queue
+     */
+    public int size() {
+        return N;
+    }
+
+    /**
+     * Returns a largest key on this priority queue.
+     *
+     * @return a largest key on this priority queue
+     * @throws NoSuchElementException if this priority queue is empty
+     */
+    public Key max() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        return pq[1];
+    }
+
+    // helper function to double the size of the heap array
+    private void resize(int capacity) {
+        assert capacity > N;
+        Key[] temp = (Key[]) new Object[capacity];
+        for (int i = 1; i <= N; i++) {
+            temp[i] = pq[i];
+        }
+        pq = temp;
+    }
+
+
+    /**
+     * Adds a new key to this priority queue.
+     *
+     * @param  x the new key to add to this priority queue
+     */
+    public void insert(Key x) {
+
+        // double size of array if necessary
+        if (N >= pq.length - 1) resize(2 * pq.length);
+
+        // add x, and percolate it up to maintain heap invariant
+        pq[++N] = x;
+        swim(N);
+        assert isMaxHeap();
+    }
+
+    /**
+     * Removes and returns a largest key on this priority queue.
+     *
+     * @return a largest key on this priority queue
+     * @throws NoSuchElementException if this priority queue is empty
+     */
+    public Key delMax() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        Key max = pq[1];
+        exch(1, N--);
+        sink(1);
+        pq[N+1] = null;     // to avoid loiterig and help with garbage collection
+        if ((N > 0) && (N == (pq.length - 1) / 4)) resize(pq.length / 2);
+        assert isMaxHeap();
+        return max;
+    }
+
+
+   /***************************************************************************
+    * Helper functions to restore the heap invariant.
+    ***************************************************************************/
+
+    private void swim(int k) {
+        while (k > 1 && less(k/2, k)) {
+            exch(k, k/2);
+            k = k/2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2*k <= N) {
+            int j = 2*k;
+            if (j < N && less(j, j+1)) j++;
+            if (!less(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+
+   /***************************************************************************
+    * Helper functions for compares and swaps.
+    ***************************************************************************/
+    private boolean less(int i, int j) {
+        if (comparator == null) {
+            return ((Comparable<Key>) pq[i]).compareTo(pq[j]) < 0;
+        }
+        else {
+            return comparator.compare(pq[i], pq[j]) < 0;
+        }
+    }
+
+    private void exch(int i, int j) {
+        Key swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+    }
+
+    // is pq[1..N] a max heap?
+    private boolean isMaxHeap() {
+        return isMaxHeap(1);
+    }
+
+    // is subtree of pq[1..N] rooted at k a max heap?
+    private boolean isMaxHeap(int k) {
+        if (k > N) return true;
+        int left = 2*k, right = 2*k + 1;
+        if (left  <= N && less(k, left))  return false;
+        if (right <= N && less(k, right)) return false;
+        return isMaxHeap(left) && isMaxHeap(right);
+    }
+
+
+   /***************************************************************************
+    * Iterator.
+    ***************************************************************************/
+
+    /**
+     * Returns an iterator that iterates over the keys on this priority queue
+     * in descending order.
+     * The iterator doesn't implement <tt>remove()</tt> since it's optional.
+     *
+     * @return an iterator that iterates over the keys in descending order
+     */
+    public Iterator<Key> iterator() {
+        return new HeapIterator();
+    }
+
+    private class HeapIterator implements Iterator<Key> {
+
+        // create a new pq
+        private MaxPQ<Key> copy;
+
+        // add all items to copy of heap
+        // takes linear time since already in heap order so no keys move
+        public HeapIterator() {
+            if (comparator == null) copy = new MaxPQ<Key>(size());
+            else                    copy = new MaxPQ<Key>(size(), comparator);
+            for (int i = 1; i <= N; i++)
+                copy.insert(pq[i]);
+        }
+
+        public boolean hasNext()  { return !copy.isEmpty();                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Key next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return copy.delMax();
+        }
+    }
+
+    /**
+     * Unit tests the <tt>MaxPQ</tt> data type.
+     */
+    public static void main(String[] args) {
+        MaxPQ<String> pq = new MaxPQ<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) pq.insert(item);
+            else if (!pq.isEmpty()) StdOut.print(pq.delMax() + " ");
+        }
+        StdOut.println("(" + pq.size() + " left on pq)");
+    }
+
 }
 
-PRIORITY QUEUES IMPLEMENTATION COST SUMMARY
-order-of-growth of running time for priority queue with N items
-implementation | insert | del max  | max
----------------+--------+----------+------
-unordered array|    1   |     N    |   N
-ordered array  |    N   |     1    |   1
-binary heap    |  log N |  log N   |   1 <==
-d'ary heap     |log_d(N)|d*log_d(N)|   1
-Fibonacci      |    1   |  log N(*)|   1 *amortized
-impossible     |    1   |     1    |   1
+/******************************************************************************
+ *  Copyright 2002-2015, Robert Sedgewick and Kevin Wayne.
+ *
+ *  This file is part of algs4.jar, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu
+ *
+ *
+ *  algs4.jar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  algs4.jar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ ******************************************************************************/
 
-Possible improvements(slight possible):
-1. d'way might work out better depending on freq on certain del max operations
-2. Fibonacci (adv structure).  Too complicated to use in practice
-
-BINARY HEAP CONSIDERATIONS: (18:11)
-Immutabililty of keys.
-* Assumption: client does not change keys while they're on the PQ.
-* Best practice: use immutable keys.
-Underflow and overflow (18:50)
-* Underflow: throw exception if deleting from empty PQ.
-* Overflow: add no-arg constructor and use resizing array.
-  resizing array leads to log N amortized time per op (how to make worst case?)
-Minimum-oriented priority queue (19:07)
-Usually we provide two implementations. Second imp:
-* Replace less() with greater()
-* Implement greater()
-Other operations for the future. (19:17)
-Can implement with sink() and swim() [stay tuned]
-* Aff the abillity to Remove an arbitrary item.
-* Give the Client the ability to Change the priority of an item.
-
-Immutable: String, Integer, Double, Color, Vector, Transaction, Point2D
-Mutable: StringBuilder, Stack, Counter, Java array
-
-Immutable Advantages: (22:03)
-* Simplfies debugging
-* Safer in presence of hostile code
-* Simplifies concurren programming
-* Safe to use as key in priority queue or symbol table +++++!!!!
-
-
-
+// Last updated: Thu Sep 24 14:16:15 EDT 2015
