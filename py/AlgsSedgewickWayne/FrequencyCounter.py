8a2addd312c990f2ffef5f7bfaa380f41e44d9ee
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2015-09-28 13:18:09 -0400

    Begining work on ESTs.

--- py/AlgsSedgewickWayne/FrequencyCounter.py
+++ py/AlgsSedgewickWayne/FrequencyCounter.py
@@ -1,102 +1,35 @@
-#!/usr/bin/env python#*****************************************************************************
- #  Compilation:  javac FrequencyCounter.java
- #  Execution:    java FrequencyCounter L < input.txt
- #  Dependencies: ST.java StdIn.java StdOut.java
- #  Data files:   http://algs4.cs.princeton.edu/31elementary/tnyTale.txt
- #                http://algs4.cs.princeton.edu/31elementary/tale.txt
- #                http://algs4.cs.princeton.edu/31elementary/leipzig100K.txt
- #                http://algs4.cs.princeton.edu/31elementary/leipzig300K.txt
- #                http://algs4.cs.princeton.edu/31elementary/leipzig1M.txt
- #
- #  Read in a list of words from standard input and print out
- #  the most frequently occurring word that has length greater than
- #  a given threshold.
- #
- #  % java FrequencyCounter 1 < tinyTale.txt
- #  it 10
- #
- #  % java FrequencyCounter 8 < tale.txt
- #  business 122
- #
- #  % java FrequencyCounter 10 < leipzig1M.txt
- #  government 24763
- #
- #
- #*****************************************************************************/
+#!/usr/bin/env python
+# TBD: DO PYTHON PORT
 
-package edu.princeton.cs.algs4
+# Reads in a command-line integer and sequence of words from
+# standard input and prints out a word (whose length exceeds
+# the threshold) that occurs most frequently to standard output.
+# It also prints out the number of words whose length exceeds
+# the threshold and the number of distinct such words.
+def main():
+  distinct = 0, words = 0
+  minlen = Integer.parseInt(args[0])
+  st = ST()
 
-#*
- #  The <tt>FrequencyCounter</tt> class provides a client for 
- #  reading in a sequence of words and printing a word (exceeding
- #  a given length) that occurs most frequently. It is useful as
- #  a test client for various symbol table implementations.
- #  <p>
- #  For additional documentation, see <a href="http://algs4.cs.princeton.edu/31elementary">Section 3.1</a> of
- #  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- #
- #  @author Robert Sedgewick
- #  @author Kevin Wayne
- #/
-public class FrequencyCounter:
+  # compute frequency counts
+  while (!StdIn.isEmpty()):
+    String key = StdIn.readString()
+    if len(key)() < minlen) continue
+    words += 1
+    if st.contains(key)):
+      st.put(key, st.get(key) + 1)
+    else:
+      st.put(key, 1)
+      distinct += 1
 
-    # Do not instantiate.
-    private FrequencyCounter(): }
+  # find a key with the highest frequency count
+  String max_key = ""
+  st.put(max_key, 0)
+  for word in st.keys():
+    if st.get(word) > st.get(max_key):
+      max_key = word
 
-    #*
-     # Reads in a command-line integer and sequence of words from
-     # standard input and prints out a word (whose length exceeds
-     # the threshold) that occurs most frequently to standard output.
-     # It also prints out the number of words whose length exceeds
-     # the threshold and the number of distinct such words.
-     #/
-    def main(String[] args):
-        distinct = 0, words = 0
-        minlen = Integer.parseInt(args[0])
-        ST<String, Integer> st = new ST<String, Integer>()
+  prt.write("{} {}\n".format(max_key, st.get(max_key)))
+  prt.write("distinct = {}\n".format(distinct))
+  prt.write("words    = {}\n".format(words))
 
-        # compute frequency counts
-        while (!StdIn.isEmpty()):
-            String key = StdIn.readString()
-            if len(key)() < minlen) continue
-            words += 1
-            if st.contains(key)):
-                st.put(key, st.get(key) + 1)
-            else:
-                st.put(key, 1)
-                distinct += 1
-
-        # find a key with the highest frequency count
-        String max = ""
-        st.put(max, 0)
-        for (String word : st.keys()):
-            if st.get(word) > st.get(max))
-                max = word
-
-        prt.write(max + " " + st.get(max))
-        prt.write("distinct = " + distinct)
-        prt.write("words    = " + words)
-
-#*****************************************************************************
- #  Copyright 2002-2015, Robert Sedgewick and Kevin Wayne.
- #
- #  This file is part of algs4.jar, which accompanies the textbook
- #
- #      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- #      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- #      http://algs4.cs.princeton.edu
- #
- #
- #  algs4.jar is free software: you can redistribute it and/or modify
- #  it under the terms of the GNU General Public License as published by
- #  the Free Software Foundation, either version 3 of the License, or
- #  (at your option) any later version.
- #
- #  algs4.jar is distributed in the hope that it will be useful,
- #  but WITHOUT ANY WARRANTY; without even the implied warranty of
- #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- #  GNU General Public License for more details.
- #
- #  You should have received a copy of the GNU General Public License
- #  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
- #*****************************************************************************/
