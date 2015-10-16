b8293d789a8eb98ef551d8f62a030515fbab405e
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2015-10-16 14:23:38 -0400

    minor

--- py/AlgsSedgewickWayne/SET.py
+++ py/AlgsSedgewickWayne/SET.py
@@ -1,300 +1,202 @@
-#!/usr/bin/env python#*****************************************************************************
- #  Compilation:  javac SET.java
- #  Execution:    java SET
- #  Dependencies: StdOut.java
- #  
- #  Set implementation using Java's TreeSet library.
- #  Does not allow duplicates.
- #
- #  % java SET
- #  128.112.136.11
- #  208.216.181.15
- #  null
- #
- #*****************************************************************************/
+"""Ordered self._treeset of keys implmented with a Balanced Binary Search Tree."""
 
-package edu.princeton.cs.algs4
+# import java.util.Iterator
+# import java.util.NoSuchElementException
+# import java.util.TreeSet
 
-import java.util.Iterator
-import java.util.NoSuchElementException
-import java.util.TreeSet
+class SET(object):
 
-#*
- #  The <tt>SET</tt> class represents an ordered set of comparable keys.
- #  It supports the usual <em>add</em>, <em>contains</em>, and <em>delete</em>
- #  methods. It also provides ordered methods for finding the <em>minimum</em>,
- #  <em>maximum</em>, <em>floor</em>, and <em>ceiling</em> and set methods
- #  for <em>union</em>, <em>intersection</em>, and <em>equality</em>.
- #  <p>
- #  Even though this implementation include the method <tt>equals()</tt>, it
- #  does not support the method <tt>hashCode()</tt> because sets are mutable.
- #  <p>
- #  This implementation uses a balanced binary search tree. It requires that
- #  the key type implements the <tt>Comparable</tt> interface and calls the
- #  <tt>compareTo()</tt> and method to compare two keys. It does not call either
- #  <tt>equals()</tt> or <tt>hashCode()</tt>.
- #  The <em>add</em>, <em>contains</em>, <em>delete</em>, <em>minimum</em>,
- #  <em>maximum</em>, <em>ceiling</em>, and <em>floor</em> methods take
- #  logarithmic time in the worst case.
- #  The <em>size</em>, and <em>is-empty</em> operations take constant time.
- #  Construction takes constant time.
- #  <p>
- #  This implementation uses a balanced binary search tree. It requires that
- #  For additional documentation, see
- #  <a href="http://algs4.cs.princeton.edu/35applications">Section 3.5</a> of
- #  <i>Algorithms in Java, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- #
- #  @author Robert Sedgewick
- #  @author Kevin Wayne
- #
- #  @param <Key> the generic type of a key in this set
- #/
+  def __init__(self):
+    self._treeset = TreeSet()
 
-public class SET< extends Comparable<>> implements Iterable<>:
-    private TreeSet<> set
+  def add(self, key):
+    """Adds the key to this treeset (if it is not already present)."""
+     if key is None: raise new NullPointerException("called add() with a None key")
+     self._treeset.add(key)
 
-    #*
-     # Initializes an empty set.
-     #/
-    public SET():
-        set = new TreeSet<>()
+  def contains(self, key):
+    """Returns true if this treeset contains the given key."""
+    if key is None: raise new NullPointerException("called contains() with a None key")
+    return self._treeset.contains(key)
 
-    #*
-     # Adds the key to this set (if it is not already present).
-     #
-     # @param  key the key to add
-     # @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
-     #/
-    def add( key):
-        if key is None) raise new NullPointerException("called add() with a None key")
-        set.add(key)
+  def delete(self, key):
+    """Removes the key from this treeset (if the key is present)."""
+    if key is None: raise new NullPointerException("called delete() with a None key")
+    self._treeset.remove(key)
 
+  def size(self): return self._treeset.size()
+  def isEmpty(self): return size() == 0
 
-    #*
-     # Returns true if this set contains the given key.
-     #
-     # @param  key the key
-     # @return <tt>true</tt> if this set contains <tt>key</tt> and
-     #         <tt>false</tt> otherwise
-     # @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
-     #/
-    def contains( key):
-        if key is None) raise new NullPointerException("called contains() with a None key")
-        return set.contains(key)
+  def iterator(self):
+    """Returns all of the keys in this treeset, as an iterator."""
+    return self._treeset.iterator()
 
-    #*
-     # Removes the key from this set (if the key is present).
-     #
-     # @param  key the key
-     # @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
-     #/
-    def delete( key):
-        if key is None) raise new NullPointerException("called delete() with a None key")
-        set.remove(key)
+  def max(self):
+    """Returns the largest key in this treeset."""
+    if isEmpty()) raise new NoSuchElementException("called max() with empty self._treeset")
+    return self._treeset.last()
 
-    #*
-     # Returns the number of keys in this set.
-     #
-     # @return the number of keys in this set
-     #/
-    def size():
-        return set.size()
+  def min(self):
+    """Returns the smallest key in this treeset."""
+    if isEmpty()) raise new NoSuchElementException("called min() with empty self._treeset")
+    return self._treeset.first()
 
-    #*
-     # Returns true if this set is empty.
-     #
-     # @return <tt>true</tt> if this set is empty, and <tt>false</tt> otherwise
-     #/
-    def isEmpty():
-        return size() == 0
- 
-    #*
-     # Returns all of the keys in this set, as an iterator.
-     # To iterate over all of the keys in a set named <tt>set</tt>, use the
-     # foreach notation: <tt>for (Key key : set)</tt>.
-     #
-     # @return an iterator to all of the keys in this set
-     #/
-    def iterator():
-        return set.iterator()
+  #*
+   #
+   # @param  key the key
+   # @return the smallest key in this self._treeset greater than or equal to <tt>key</tt>
+   # @throws NoSuchElementException if there is no such key
+   # @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+   #/
+  public  ceiling( key):
+    """Returns the smallest key in this treeset greater than or equal to key."""
+      if key is None) raise new NullPointerException("called ceiling() with a None key")
+       k = self._treeset.ceiling(key)
+      if k is None) raise new NoSuchElementException("all keys are less than " + key)
+      return k
 
-    #*
-     # Returns the largest key in this set.
-     #
-     # @return the largest key in this set
-     # @throws NoSuchElementException if this set is empty
-     #/
-    public  max():
-        if isEmpty()) raise new NoSuchElementException("called max() with empty set")
-        return set.last()
+  #*
+   # Returns the largest key in this self._treeset less than or equal to <tt>key</tt>.
+   #
+   # @param  key the key
+   # @return the largest key in this self._treeset table less than or equal to <tt>key</tt>
+   # @throws NoSuchElementException if there is no such key
+   # @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
+   #/
+  public  floor( key):
+      if key is None) raise new NullPointerException("called floor() with a None key")
+       k = self._treeset.floor(key)
+      if k is None) raise new NoSuchElementException("all keys are greater than " + key)
+      return k
 
-    #*
-     # Returns the smallest key in this set.
-     #
-     # @return the smallest key in this set
-     # @throws NoSuchElementException if this set is empty
-     #/
-    public  min():
-        if isEmpty()) raise new NoSuchElementException("called min() with empty set")
-        return set.first()
+  #*
+   # Returns the union of this self._treeset and that self._treeset.
+   #
+   # @param  that the other self._treeset
+   # @return the union of this self._treeset and that self._treeset
+   # @throws NullPointerException if <tt>that</tt> is <tt>null</tt>
+   #/
+  def union(SET<> that):
+      if that is None) raise new NullPointerException("called union() with a None argument")
+      SET<> c = new SET<>()
+      for ( x : this):
+          c.add(x)
+      for ( x : that):
+          c.add(x)
+      return c
 
+  #*
+   # Returns the intersection of this self._treeset and that self._treeset.
+   #
+   # @param  that the other self._treeset
+   # @return the intersection of this self._treeset and that self._treeset
+   # @throws NullPointerException if <tt>that</tt> is <tt>null</tt>
+   #/
+  def intersects(SET<> that):
+      if that is None) raise new NullPointerException("called intersects() with a None argument")
+      SET<> c = new SET<>()
+      if this.size() < that.size()):
+          for ( x : this):
+              if that.contains(x)) c.add(x)
+      else:
+          for ( x : that):
+              if this.contains(x)) c.add(x)
+      return c
 
-    #*
-     # Returns the smallest key in this set greater than or equal to <tt>key</tt>.
-     #
-     # @param  key the key
-     # @return the smallest key in this set greater than or equal to <tt>key</tt>
-     # @throws NoSuchElementException if there is no such key
-     # @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
-     #/
-    public  ceiling( key):
-        if key is None) raise new NullPointerException("called ceiling() with a None key")
-         k = set.ceiling(key)
-        if k is None) raise new NoSuchElementException("all keys are less than " + key)
-        return k
+  #*       
+   # Compares this self._treeset to the specified self._treeset.
+   # <p>
+   # Note that this method declares two empty sets to be equal
+   # even if they are parameterized by different generic types.
+   # This is consistent with the behavior of <tt>equals()</tt> 
+   # within Java's Collections framework.
+   #       
+   # @param  other the other self._treeset
+   # @return <tt>true</tt> if this self._treeset equals <tt>other</tt>;
+   #         <tt>false</tt> otherwise
+   #/
+  @Override
+  def equals(self, other):
+      if other == this) return True
+      if other is None) return False
+      if other.getClass() != this.getClass()) return False
+      SET<> that = (SET<>) other
+      if this.size() != that.size()) return False
+      try:
+          for ( k : this)
+              if !that.contains(k)) return False
+      catch (ClassCastException exception):
+          return False
+      return True
 
-    #*
-     # Returns the largest key in this set less than or equal to <tt>key</tt>.
-     #
-     # @param  key the key
-     # @return the largest key in this set table less than or equal to <tt>key</tt>
-     # @throws NoSuchElementException if there is no such key
-     # @throws NullPointerException if <tt>key</tt> is <tt>null</tt>
-     #/
-    public  floor( key):
-        if key is None) raise new NullPointerException("called floor() with a None key")
-         k = set.floor(key)
-        if k is None) raise new NoSuchElementException("all keys are greater than " + key)
-        return k
+  #*
+   # This operation is not supported because sets are mutable.
+   #
+   # @return does not return a value
+   # @throws UnsupportedOperationException if called
+   #/
+  @Override
+  def hashCode(self):
+      raise new UnsupportedOperationException("hashCode() is not supported because sets are mutable")
 
-    #*
-     # Returns the union of this set and that set.
-     #
-     # @param  that the other set
-     # @return the union of this set and that set
-     # @throws NullPointerException if <tt>that</tt> is <tt>null</tt>
-     #/
-    def union(SET<> that):
-        if that is None) raise new NullPointerException("called union() with a None argument")
-        SET<> c = new SET<>()
-        for ( x : this):
-            c.add(x)
-        for ( x : that):
-            c.add(x)
-        return c
+  #*
+   # Returns a string representation of this self._treeset.
+   #
+   # @return a string representation of this self._treeset, with the keys separated
+   #         by single spaces
+   #/
+  @Override
+  def toString(self):
+      StringBuilder s = new StringBuilder()
+      for ( key : this)
+          s.append(key + " ")
+      return s.toString()
 
-    #*
-     # Returns the intersection of this set and that set.
-     #
-     # @param  that the other set
-     # @return the intersection of this set and that set
-     # @throws NullPointerException if <tt>that</tt> is <tt>null</tt>
-     #/
-    def intersects(SET<> that):
-        if that is None) raise new NullPointerException("called intersects() with a None argument")
-        SET<> c = new SET<>()
-        if this.size() < that.size()):
-            for ( x : this):
-                if that.contains(x)) c.add(x)
-        else:
-            for ( x : that):
-                if this.contains(x)) c.add(x)
-        return c
-
-    #*       
-     # Compares this set to the specified set.
-     # <p>
-     # Note that this method declares two empty sets to be equal
-     # even if they are parameterized by different generic types.
-     # This is consistent with the behavior of <tt>equals()</tt> 
-     # within Java's Collections framework.
-     #       
-     # @param  other the other set
-     # @return <tt>true</tt> if this set equals <tt>other</tt>;
-     #         <tt>false</tt> otherwise
-     #/
-    @Override
-    def equals( other):
-        if other == this) return True
-        if other is None) return False
-        if other.getClass() != this.getClass()) return False
-        SET<> that = (SET<>) other
-        if this.size() != that.size()) return False
-        try:
-            for ( k : this)
-                if !that.contains(k)) return False
-        catch (ClassCastException exception):
-            return False
-        return True
-
-    #*
-     # This operation is not supported because sets are mutable.
-     #
-     # @return does not return a value
-     # @throws UnsupportedOperationException if called
-     #/
-    @Override
-    def hashCode():
-        raise new UnsupportedOperationException("hashCode() is not supported because sets are mutable")
-
-    #*
-     # Returns a string representation of this set.
-     #
-     # @return a string representation of this set, with the keys separated
-     #         by single spaces
-     #/
-    @Override
-    def toString():
-        StringBuilder s = new StringBuilder()
-        for ( key : this)
-            s.append(key + " ")
-        return s.toString()
-
-    #*
-     # Unit tests the <tt>SET</tt> data type.
-     #/
-    def main(String[] args):
-        SET<String> set = new SET<String>()
+  #*
+   # Unit tests the <tt>SET</tt> data type.
+   #/
+  def main(String[] args):
+      SET<String> self._treeset = new SET<String>()
 
 
-        # insert some keys
-        set.add("www.cs.princeton.edu")
-        set.add("www.cs.princeton.edu");    # overwrite old value
-        set.add("www.princeton.edu")
-        set.add("www.math.princeton.edu")
-        set.add("www.yale.edu")
-        set.add("www.amazon.com")
-        set.add("www.simpsons.com")
-        set.add("www.stanford.edu")
-        set.add("www.google.com")
-        set.add("www.ibm.com")
-        set.add("www.apple.com")
-        set.add("www.slashdot.com")
-        set.add("www.whitehouse.gov")
-        set.add("www.espn.com")
-        set.add("www.snopes.com")
-        set.add("www.movies.com")
-        set.add("www.cnn.com")
-        set.add("www.iitb.ac.in")
+      # insert some keys
+      self._treeset.add("www.cs.princeton.edu")
+      self._treeset.add("www.cs.princeton.edu");    # overwrite old value
+      self._treeset.add("www.princeton.edu")
+      self._treeset.add("www.math.princeton.edu")
+      self._treeset.add("www.yale.edu")
+      self._treeset.add("www.amazon.com")
+      self._treeset.add("www.simpsons.com")
+      self._treeset.add("www.stanford.edu")
+      self._treeset.add("www.google.com")
+      self._treeset.add("www.ibm.com")
+      self._treeset.add("www.apple.com")
+      self._treeset.add("www.slashdot.com")
+      self._treeset.add("www.whitehouse.gov")
+      self._treeset.add("www.espn.com")
+      self._treeset.add("www.snopes.com")
+      self._treeset.add("www.movies.com")
+      self._treeset.add("www.cnn.com")
+      self._treeset.add("www.iitb.ac.in")
 
 
-        prt.write(set.contains("www.cs.princeton.edu"))
-        prt.write(!set.contains("www.harvardsucks.com"))
-        prt.write(set.contains("www.simpsons.com"))
-        prt.write()
+      prt.write(self._treeset.contains("www.cs.princeton.edu"))
+      prt.write(!self._treeset.contains("www.harvardsucks.com"))
+      prt.write(self._treeset.contains("www.simpsons.com"))
+      prt.write()
 
-        prt.write("ceiling(www.simpsonr.com) = " + set.ceiling("www.simpsonr.com"))
-        prt.write("ceiling(www.simpsons.com) = " + set.ceiling("www.simpsons.com"))
-        prt.write("ceiling(www.simpsont.com) = " + set.ceiling("www.simpsont.com"))
-        prt.write("floor(www.simpsonr.com)   = " + set.floor("www.simpsonr.com"))
-        prt.write("floor(www.simpsons.com)   = " + set.floor("www.simpsons.com"))
-        prt.write("floor(www.simpsont.com)   = " + set.floor("www.simpsont.com"))
-        prt.write()
+      prt.write("ceiling(www.simpsonr.com) = " + self._treeset.ceiling("www.simpsonr.com"))
+      prt.write("ceiling(www.simpsons.com) = " + self._treeset.ceiling("www.simpsons.com"))
+      prt.write("ceiling(www.simpsont.com) = " + self._treeset.ceiling("www.simpsont.com"))
+      prt.write("floor(www.simpsonr.com)   = " + self._treeset.floor("www.simpsonr.com"))
+      prt.write("floor(www.simpsons.com)   = " + self._treeset.floor("www.simpsons.com"))
+      prt.write("floor(www.simpsont.com)   = " + self._treeset.floor("www.simpsont.com"))
+      prt.write()
 
 
-        # print out all keys in this set in lexicographic order
-        for (String s : set):
-            prt.write(s)
+      # print out all keys in this self._treeset in lexicographic order
+      for (String s : self._treeset):
+          prt.write(s)
 
 
 
--- thirdparty/java2tmp.py
+++ thirdparty/java2tmp.py
@@ -14,7 +14,8 @@ def main():
       if chk_com(FOUT, r'^(\s*)\/\*(.*)$', line): continue
       if chk_com(FOUT, r'^(\s*)\*(.*)$',   line): continue
       if chk_end(FOUT, line): continue
-      line = chk_if(FOUT, line)
+      if line[:4] == "    ": line = line[2:]
+      line = chk_if(line)
       line = line.replace('== null', 'is None')
       line = line.replace('!= null', 'is not None')
       line = line.replace('true',  'True')
@@ -32,11 +33,11 @@ def main():
       line = line.replace(r'||', 'or')
       line = line.replace(r'throw', 'raise')
       line = line.replace(r'null', 'None')
-      line = chk_semi(FOUT, line)
-      line = chk_start(FOUT, line)
-      line = chk_private(FOUT, line)
-      line = chk_public(FOUT, line)
-      line = chk_else(FOUT, line)
+      line = chk_semi(line)
+      line = chk_start(line)
+      line = chk_private(line)
+      line = chk_public(line)
+      line = chk_else(line)
       line = re.sub(r'([a-zA-Z0-9]+)\.length', r'len(\1)', line)
       line = line.replace(' int ',' ')
       line = line.replace('else if', 'elif')
@@ -44,6 +45,7 @@ def main():
       line = line.replace('++', ' += 1')
       line = line.replace('--', ' -= 1')
       line = line.replace('StdOut.println', 'prt.write')
+      line = chk_def(line):
       FOUT.write(line)
 
   print 'READ ', fin
@@ -54,44 +56,48 @@ def main():
   else:
     print "VIEW ", get_foutname( fin )
 
-def chk_else(FOUT, line):
+def chk_def(line):
+  if "def " in line:
+    pass
+
+def chk_else(line):
  A = re.search(r'^(\s*)else(\s+)(return\s+.*)$', line)
  if A:
    return '{}else:{}{}\n'.format(A.group(1), A.group(2), A.group(3))
  return line
 
-def chk_public(FOUT, line):
+def chk_public(line):
  A = re.search(r'^(\s*)(public .*\S\s+)([a-zA-Z0-1_]+)\s*(\(.*)$', line)
  if A:
    return '{}def {}{}\n'.format(A.group(1), A.group(3), A.group(4))
  return line
 
-def chk_private(FOUT, line):
+def chk_private(line):
  A = re.search(r'^(\s*)(private .*\S\s+)([a-zA-Z0-1_]+)\s*(\(.*)$', line)
  if A:
    return '{}def _{}{}\n'.format(A.group(1), A.group(3), A.group(4))
  return line
 
-def chk_if(FOUT, line):
+def chk_if(line):
  A = re.search(r'^(\s*if\s+)\((.*)$', line)
  if A:
    return '{}{}\n'.format(A.group(1), A.group(2))
  return line
 
-def chk_start(FOUT, line):
+def chk_start(line):
  A = re.search(r'^(.*\S)(\s*{)(.*)$', line)
  if A:
    return '{}:{}\n'.format(A.group(1), A.group(3))
  return line
 
-def chk_semi(FOUT, line):
+def chk_semi(line):
  A = re.search(r'^(.*)\s*;\s*$', line)
  if A:
    return '{}\n'.format(A.group(1))
  return line
 
 
-def chk_end(FOUT, line):
+def chk_end(line):
  A = re.search(r'^\s*}\s*$', line)
  if A:
    return True
