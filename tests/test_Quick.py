39b77bbd41019ab6fac00982a8b5d51627599919
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2015-10-20 09:43:22 -0400

    cli

--- tests/test_Quick.py
+++ tests/test_Quick.py
@@ -26,33 +26,35 @@ def test_1(self):
 
 def test_stdin():
   """echo "a b c d e f" | test_Quick.py"""
-  a = cli()
-  print a
+  a = cli_get_array()
+  print a[:3]
   print len(a)
 
-def cli():
+def cli_get_array(seqstr=None):
   """Command-line interface: reads data from arg, stdin, stream, or files."""
-  # $ [file.py] "A B C D E F"
-  if len(sys.argv) == 2:
-    a = arr_int_or_str(sys.argv[1].split(" "))
+  if seqstr is not None:
+    return arr_int_str(seqstr)
+  # >>> [file.py] "A B C D E F"
+  # >>> [file.py] "1 2 3 4 5 6"
+  if len(sys.argv) == 2 and not os.path.isfile(sys.argv[1]):
+    a = arr_int_str(sys.argv[1].split(" "))
     if a is not None:
       return a
-  # $ echo "A B C D E F" | [file.py]
-  # $ [file.py] # And then enter elems 1 at a time on stdin. End with two ctrl-Ds
+  # >>> echo "A B C D E F" | [file.py]
+  # >>> echo "1 2 3 4 5 6" | [file.py]
+  # >>> [file.py] # And then enter elems 1 at a time on stdin. End with two ctrl-Ds
   a = [w.strip(" \n\r") for t in fileinput.input() for w in t.splitlines()]
   if len(a) == 1 and re.search(r'[(\S+\s+)]+', a[0]):
-    return arr_int_or_str(a[0].split(" "))
-  # $ test_Quick.py ../thirdparty/1Kints.txt
+    return arr_int_str(a[0].split(" "))
+  # >>> test_Quick.py ../thirdparty/1Kints.txt
   if a is not None:
-    return a
+    return arr_int_str(a)
 
-def arr_int_or_str(a):
+def arr_int_str(a):
   """Return an array of ints or strs."""
   isdigit = True 
   for elem in a:
-    if os.path.isfile(elem):
-      return None
-    M = re.search(r'[0-9.-eE]', elem)
+    M = re.search(r'^([0-9.\-eE ]+)$', elem)
     if not M:
       isdigit = False
       break
