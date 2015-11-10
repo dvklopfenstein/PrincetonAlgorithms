c336c7a13452e3c5f6c84914f6cfb76d31a4f96e
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2015-11-09 21:45:58 -0500

    cleanup

--- py/AlgsSedgewickWayne/DirectedEdge.py
+++ py/AlgsSedgewickWayne/DirectedEdge.py
@@ -6,14 +6,14 @@ class DirectedEdge(object):
   def __init__(self, v, w, weight):
     if v < 0: raise Exception("Vertex names must be nonnegative integers")
     if w < 0: raise Exception("Vertex names must be nonnegative integers")
-    if isinstance(weight, float): raise Exception("Weight is NaN")
+    if not isinstance(weight, float): raise Exception("Weight is NaN")
     self._v = v # the tail vertex
     self._w = w # the head vertex
     self._weight = weight # the weight of the directed edge
 
-  def from(self): return self._v # Returns the tail vertex of the directed edge.
-  def to(self): return self._w # Returns the head vertex of the directed edge.
-  def weight(self): return self._weight # Returns the weight of the directed edge.
+  def get_from(self): return self._v # Returns the tail vertex of the directed edge.
+  def get_to(self): return self._w # Returns the head vertex of the directed edge.
+  def get_weight(self): return self._weight # Returns the weight of the directed edge.
   def __str__(self):
     return "{v} -> {w} {wt:5.2f}".format(v=self._v, w=self._w, wt=self._weight)
 
--- tests/test_DirectedEdge.py
+++ tests/test_DirectedEdge.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 from AlgsSedgewickWayne.DirectedEdge import DirectedEdge
 import sys
