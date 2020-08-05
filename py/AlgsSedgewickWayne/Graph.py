026ea37dc98d67880304cd4f04f941eb4582dae5
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2020-08-05 10:17:05 -0400

    pass pylint

--- py/AlgsSedgewickWayne/Graph.py
+++ py/AlgsSedgewickWayne/Graph.py
@@ -3,121 +3,142 @@
 import sys
 from AlgsSedgewickWayne.testcode.utils import adjtxtblk2OrderedDict
 
-class Graph(object):
+class Graph:
+    """Undirected graph"""
 
-  def __init__(self, a=None, **kwargs):
-    if a is not None:
-      if isinstance(a, int):
-        self._init_empty(a)
-      elif len(a) == 1:
-        self._init_empty(a[0])
-      else:
-        self._init(a)
-      self.keys = range(self._V)
-    elif 'adjtxt' in kwargs:
-      self._adj = adjtxtblk2OrderedDict(kwargs['adjtxt'])
-      self._V = len(self._adj)
-      self._E = len(set([tuple(sorted([v, w])) for v, ws in self._adj.items() for w in ws]))
-      self.keys = self._adj.keys()
+    def __init__(self, arg=None, **kwargs):
+        if arg is not None:
+            if isinstance(arg, int):
+                self._init_empty(arg)
+            elif len(arg) == 1:
+                self._init_empty(arg[0])
+            else:
+                self._init(arg)
+            self.keys = range(self.num_nodes)
+        elif 'adjtxt' in kwargs:
+            self._adj = adjtxtblk2OrderedDict(kwargs['adjtxt'])
+            self.num_nodes = len(self._adj)
+            self.num_edges = self._init_num_edges()
+            self.keys = self._adj.keys()
 
-  def _init_empty(self, V):
-    if V < 0: raise Exception("Number of vertices must be nonnegative")
-    self._V = V
-    self._E = 0
-    self._adj = [set() for v in range(V)]
+    def V(self):
+        """Returns the number of vertices in self graph."""
+        return self.num_nodes
 
-  def _init(self, a):
-    """Initializes a graph from an input stream."""
-    # The format is the number of vertices V, followed by the number of edges E,
-    # followed by E pairs of vertices, with each entry separated by whitespace.
-    self._init_empty(a[0]) # init V, and the empty adj list
-    E = a[1]
-    if E < 0: raise Exception("Number of edges must be nonnegative")
-    for v, w in a[2:]:
-      self.addEdge(v, w)
+    def E(self):
+        """Returns the number of edges in self graph."""
+        return self.num_edges
 
-  def V(self): return self._V # Returns the number of vertices in self graph.
-  def num_nodes(self): return self._V # Returns the number of vertices in self graph.
-  def E(self): return self._E # Returns the number of edges in self graph.
-  def num_edges(self): return self._E # Returns the number of edges in self graph.
+    def addEdge(self, node_v, node_w):
+        """Adds the undirected edge node_v-node_w to self graph."""
+        self.add_edge(node_v, node_w)
 
-  def addEdge(self, v, w):
-    """Adds the undirected edge v-w to self graph."""
-    #self._validateVertex(v)
-    #self._validateVertex(w)
-    self._E += 1
-    self._adj[v].add(w)
-    self._adj[w].add(v)
+    def add_edge(self, node_v, node_w):
+        """Adds the undirected edge node_v-node_w to self graph."""
+        #self._validateVertex(node_v)
+        #self._validateVertex(node_w)
+        self.num_edges += 1
+        self._adj[node_v].add(node_w)
+        self._adj[node_w].add(node_v)
 
-  def adj(self, v):
-    """Returns the vertices adjacent to vertex v."""
-    #self._validateVertex(v)
-    return self._adj[v]
+    def adj(self, node_v):
+        """Returns the vertices adjacent to vertex node_v."""
+        #self._validateVertex(node_v)
+        return self._adj[node_v]
 
-  def degree(self, v):
-    """Returns the degree of vertex v."""
-    #self._validateVertex(v)
-    return self._adj[v].size()
+    def degree(self, node_v):
+        """Returns the degree of vertex node_v."""
+        #self._validateVertex(node_v)
+        return self._adj[node_v].size()
 
-  #def _validateVertex(self, v):
-  #  """raise an IndexOutOfBoundsException unless 0 <= v < V."""
-  #  if v < 0 or v >= self._V or v not in self._adj:
-  #    raise Exception("vertex {} is not between 0 and {} or in {}".format(v, self._V-1, self._adj))
+    #def _validateVertex(self, node_v):
+    #  """raise an IndexOutOfBoundsException unless 0 <= node_v < V."""
+    #  if node_v < 0 or node_v >= self.num_nodes or node_v not in self._adj:
+    #    raise Exception("vertex {} is not between 0 and {} or in {}".format(
+    #        node_v, self.num_nodes-1, self._adj))
 
-  def __str__(self):
-    s = [(("{V} vertices, {E} edges\n").format(V=self._V, E=self._E))]
-    for v in self.keys:
-      s.append("{v}: ".format(v=v))
-      for w in self._adj[v]:
-        s.append("{w} ".format(w=w))
-      s.append("\n")
-    return ''.join(s)
+    def __str__(self):
+        txt = [(("{V} vertices, {E} edges\n").format(V=self.num_nodes, E=self.num_edges))]
+        for node_v in self.keys:
+            txt.append("{node_v}: ".format(node_v=node_v))
+            for node_w in self._adj[node_v]:
+                txt.append("{node_w} ".format(node_w=node_w))
+            txt.append("\n")
+        return ''.join(txt)
 
-  def __iter__(self): # Makes Graph an iterable.
-    return iter(self._adj) # returns an iterator.
+    def __iter__(self): # Makes Graph an iterable.
+        return iter(self._adj) # returns an iterator.
 
-  def wr_png(self, fout_png="Graph.png", prt=sys.stdout, **kwargs):
-    """Make a png showing a diagram of the connected components."""
-    import pydot
-    # 1. create/initialize graph
-    g = pydot.Dot(graph_type='graph') # undirected graph
-    # 2. create nodes
-    nodes = [pydot.Node(v) for v in self.keys]
-    # 3. add nodes to graph
-    for node in nodes:
-      g.add_node(node)
-    # 4. add edges between nodes to graph
-    for v, w in self.get_edges():
-      if v != w: # print only edges from one node to another (not to self)
-        g.add_edge(pydot.Edge(v, w))
-    # 5. write graph to png file
-    g.write_png(fout_png)
-    prt.write("  wrote: {}\n".format(fout_png))
+    def wr_png(self, fout_png="Graph.png", prt=sys.stdout, **kwargs):
+        """Make a png showing a diagram of the connected components."""
+        import pydot
+        # 1. create/initialize graph
+        graph = pydot.Dot(graph_type='graph') # undirected graph
+        # 2. create nodes
+        nodes = [pydot.Node(node_v) for node_v in self.keys]
+        # 3. add nodes to graph
+        for node in nodes:
+            graph.add_node(node)
+        # 4. add edges between nodes to graph
+        for node_v, node_w in self.get_edges():
+            if node_v != node_w: # print only edges from one node to another (not to self)
+                graph.add_edge(pydot.Edge(node_v, node_w))
+        # 5. write graph to png file
+        graph.write_png(fout_png)
+        prt.write("  wrote: {}\n".format(fout_png))
 
-  def get_edges(self):
-    edges = set()
-    for v in self.keys:
-      for w in self._adj[v]:
-        edges.add(tuple(sorted([v, w]))) 
-    return edges
+    def get_edges(self):
+        """Get edges"""
+        edges = set()
+        for node_v in self.keys:
+            for node_w in self._adj[node_v]:
+                edges.add(tuple(sorted([node_v, node_w])))
+        return edges
+
+    def _init_empty(self, num_nodes):
+        """Initialize an empty graph containing N nodes"""
+        if num_nodes < 0:
+            raise Exception("Number of vertices must be nonnegative")
+        self.num_nodes = num_nodes
+        self.num_edges = 0
+        self._adj = [set() for node_v in range(num_nodes)]
+
+    def _init(self, arg):
+        """Initializes arg graph from an input stream."""
+        # The format is the number of vertices V, followed by the number of edges E,
+        # followed by E pairs of vertices, with each entry separated by whitespace.
+        self._init_empty(arg[0]) # init V, and the empty adj list
+        edge = arg[1]
+        if edge < 0:
+            raise Exception("Number of edges must be nonnegative")
+        for node_v, node_w in arg[2:]:
+            self.add_edge(node_v, node_w)
+
+    def _init_num_edges(self):
+        """Get the number of edges"""
+        edges = set()
+        for node_v, nodes in self._adj.items():
+            for node_w in nodes:
+                edges.add(frozenset([node_v, node_w]))
+        return len(edges)
 
  #*****************************************************************************/
  #  % Graph.py ../thirdparty/tinyG.txt
- #  13 vertices, 13 edges 
- #  0: 6 2 1 5 
- #  1: 0 
- #  2: 0 
- #  3: 5 4 
- #  4: 5 6 3 
- #  5: 3 4 0 
- #  6: 0 4 
- #  7: 8 
- #  8: 7 
- #  9: 11 10 12 
- #  10: 9 
- #  11: 9 12 
- #  12: 11 9 
- #  
+ #  13 vertices, 13 edges
+ #  0: 6 2 1 5
+ #  1: 0
+ #  2: 0
+ #  3: 5 4
+ #  4: 5 6 3
+ #  5: 3 4 0
+ #  6: 0 4
+ #  7: 8
+ #  8: 7
+ #  9: 11 10 12
+ #  10: 9
+ #  11: 9 12
+ #  12: 11 9
+ #
  #*****************************************************************************/
 
 # -----------------------------------------------------------------------------
@@ -139,7 +160,7 @@ class Graph(object):
 #
 # PLANARITY: Can you draw the graph in the plane with no crossing edges?
 # GRAPH ISOMORPHISM: Do two adjacency lists represent the same graph?
-# 
+#
 # CHALLENGE: Whinc of these problems are easy? difficult? intractable?
 
 
--- tests/test_Graph.py
+++ tests/test_Graph.py
@@ -1,37 +1,46 @@
 #!/usr/bin/env python
+"""Test that Graph is implemented properly"""
 
+import sys
+from os.path import join
+from os.path import dirname
+from os.path import abspath
 from AlgsSedgewickWayne.Graph import Graph
 from AlgsSedgewickWayne.testcode.InputArgs import cli_get_fin
 
-import sys
+TEST_DIR = dirname(abspath(__file__))
 
-def run(a, prt=sys.stdout):
-  g = Graph(a)
-  prt.write("{}\n".format(g))
-  g.wr_png("Graph_test_0.png")
 
-def test_0(prt=sys.stdout):
-  run(cli_get_fin("../thirdparty/tinyG.txt"))
+def test_0():
+    """Test reading tiny Graph"""
+    graph = _run(cli_get_fin(join(TEST_DIR, "tinyG.txt")))
+    assert graph.num_nodes == 13
+    assert graph.num_edges == 13
 
 def test_1(prt=sys.stdout):
-  adjtxtblk = """
-    A:  E B 
-    B:  E A F 
-    C:  D F 
-    D:  C G H 
-    E:  A B 
-    F:  C G B 
-    G:  D H F 
-    H:  G D """
-  # Convert adjacency list in text block to array, fmt=[V, E, edge_pairs]
-  g = Graph(adjtxt=adjtxtblk)
-  prt.write("{}\n".format(g))
-  g.wr_png("Graph_test_1.png")
+    """Test that Graph is implemented properly"""
+    adjtxtblk = """
+      A:  E B 
+      B:  E A F 
+      C:  D F 
+      D:  C G H 
+      E:  A B 
+      F:  C G B 
+      G:  D H F 
+      H:  G D """
+    # Convert adjacency list in text block to array, fmt=[V, E, edge_pairs]
+    graph = Graph(adjtxt=adjtxtblk)
+    assert graph.num_nodes == 8
+    assert graph.num_edges == 10
+    prt.write("{}\n".format(graph))
+    graph.wr_png("Graph_test_1.png")
 
-def run_all():
-  test_0()
-  test_1()
+def _run(data, prt=sys.stdout):
+    graph = Graph(data)
+    prt.write("{}\n".format(graph))
+    graph.wr_png("Graph_test_0.png")
+    return graph
 
 if __name__ == '__main__':
-  #run_all()
-  test_1()
+    test_0()
+    test_1()
