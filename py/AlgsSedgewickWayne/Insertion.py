ee67cedf3c957ce575781a65d2912cb68b71996c
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2020-08-19 13:40:53 -0400

    pylint and update for Python3

--- py/AlgsSedgewickWayne/Insertion.py
+++ py/AlgsSedgewickWayne/Insertion.py
@@ -1,23 +1,25 @@
 """Alg1 Week2 Lecture Insertion Sort"""
 
-import sys
 from AlgsSedgewickWayne.utils import _isSorted, __lt__, _exch
 
-def Sort(ARR, array_history=None):
-  """Rearranges the array in ascending order, using the natural order."""
-  N = len(ARR)
-  # 00:57 Everything to the left is in acending order
-  #       Everything to the right, we have not seen at all
-  for i in range(N):
-    j = i
-    # Exchange the curr Elem with every element to the left that is > 01:21
-    while j > 0 and __lt__(ARR[j], ARR[j-1]): # Iterate from i back towards 0
-      if array_history is not None: array_history.add_history(ARR, {j:'*', j-1:'*'})
-      _exch(ARR, j, j-1)
-      j -= 1
-    assert _isSorted(ARR, 0, i)
-  assert _isSorted(ARR);
-  if array_history is not None: array_history.add_history(ARR, None)
+
+def Sort(arr, array_history=None):
+    """Rearranges the array in ascending order, using the natural order."""
+    num_elems = len(arr)
+    # 00:57 Everything to the left is in acending order
+    #       Everything to the right, we have not seen at all
+    for i in range(num_elems):
+        j = i
+        # Exchange the curr Elem with every element to the left that is > 01:21
+        while j > 0 and __lt__(arr[j], arr[j-1]): # Iterate from i back towards 0
+            if array_history is not None:
+                array_history.add_history(arr, {j:'*', j-1:'*'})
+            _exch(arr, j, j-1)
+            j -= 1
+        assert _isSorted(arr, 0, i)
+    assert _isSorted(arr)
+    if array_history is not None:
+        array_history.add_history(arr, None)
 
 
 #************************************************************************
@@ -41,7 +43,8 @@ def Sort(ARR, array_history=None):
 #  The <tt>Insertion</tt> class provides static methods for sorting an
 #  array using insertion sort.
 #  <p>
-#  For additional documentation, see <a href="http:#algs4.cs.princeton.edu/21elementary">Section 2.1</a> of
+#  For additional documentation, see :
+#      <a href="http:#algs4.cs.princeton.edu/21elementary">Section 2.1</a> of
 #  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
 #
 #  @author Robert Sedgewick
@@ -55,16 +58,18 @@ def Sort(ARR, array_history=None):
 #   * Entries to the left or Ptr (inc Ptr) are in ascending order
 #   * Entries to the right of Ptr have not yet been seen
 # PROPOSITION: 04:27 To sort a randomly-ordered array with distinct keys:
-#   ~ 1/4*N^2 compares  on average
-#   ~ 1/4*N^2 exchanges on average
+#   ~ 1/4*num_elems^2 compares  on average
+#   ~ 1/4*num_elems^2 exchanges on average
 # PROOF: 05:16 Expect each entry to move halfway back below the diagonal on the average
 #
 # INSERTION SORT: BEST CASE AND WORST CASE: 06:10
 # BEST CASE 06:12: If the array is in ascending order,
-#   insertion sort makes N-1 compares and 0 exchanges
+#   insertion sort makes num_elems-1 compares and 0 exchanges
 #   A E E L , O P R S T X
 # WORST CASE 06:37: If the array is in descending order (and no duplicates),
-#   insertion sort makes ~ 1/2*N^2 compares and ~ 1/2*N^2 exchanges (Slower that Selection sort)
+#   insertion sort makes:
+#       ~ 1/2*num_elems^2 compares and
+#       ~ 1/2*num_elems^2 exchanges (Slower that Selection sort)
 #   X T S R P O M L E E A
 #   For every step, it is not just comparing, it is also exchanging
 #
@@ -77,13 +82,13 @@ def Sort(ARR, array_history=None):
 #     2. Just a few elems out of place.
 # DEFINITION: An INVERSION is a pair of keys that are out of order:
 #   A E E L M O T R X P S (6 inversions: T-R T-P T-S R-P X-P X-S)
-# DEFINITION: An array is PARTIALLY-SORTED if the number of inversions is <= c*N
-#  * Ex 1. A subarray of size 10 appended to a sorted subarray of size N
-#  * Ex 2. An array of size N with only 10 entries out of place
+# DEFINITION: An array is PARTIALLY-SORTED if the number of inversions is <= c*num_elems
+#  * Ex 1. A subarray of size 10 appended to a sorted subarray of size num_elems
+#  * Ex 2. An array of size num_elems with only 10 entries out of place
 #
 # PROPOSITION: For partially-sorted arrays, insertion sort runs in linear time. 08:57
 # PROOF: Number of exchanges equals the numbers of inversions
-#   number of compares = exchanges + (N - 1)
+#   number of compares = exchanges + (num_elems - 1)
 #
 # INSERTION SORT IS INEFFICIENT because elements only move one place at a time
 # even if we know that they have to move far away.
@@ -91,14 +96,16 @@ def Sort(ARR, array_history=None):
 # QUESTION: How many compares does insertion sort make on an imput array that is already sorted?
 # ANSWER: linear
 #
-# QUESTION: True or False? The expected # of compares to insertion sort an array 
-# containing N/2 0s and N/2 1s in uniformly random order is ~1/4 N^2.
-# ANSWER: False. Consider element i>0. How many of the items a[0], a[1], ..., a[i-1] is a[i] inverted with?
+# QUESTION: True or False? The expected # of compares to insertion sort an array
+# containing num_elems/2 0s and num_elems/2 1s in uniformly random order is ~1/4 num_elems^2.
+# ANSWER: False.
+# Consider element i>0. How many of the items a[0], a[1], ..., a[i-1] is a[i] inverted with?
 # If a[i] == 1 (which happens with probability 1/2), then the number is 0.
 # If a[i] == 0 (which happens with probability 1/2), then we expect half of the i previous
 # elements to be 1s, so the expected number is i/2.
-# So the expected number of inversions is 1/2(0/2 + 1/2 + 2/2 + 3/2 + ... + (N-1)/2) ~N^2/8.
-# Thus, the expected number of compares is ~ 1/8 N^2
+# So the expected number of inversions is:
+#     1/2(0/2 + 1/2 + 2/2 + 3/2 + ... + (num_elems-1)/2) ~num_elems^2/8.
+# Thus, the expected number of compares is ~ 1/8 num_elems^2
 
 # TRUE: Any pair of items is compared no more than once during insertion sort.
 # EXPLANATION: Let a[i] and a[j] be two entries in the unsorted array with i<j.
@@ -109,29 +116,29 @@ def Sort(ARR, array_history=None):
 # number of compares than what the answer says, but here is 'the answer':
 #
 # Seed: 962938
-# QUESTION: The number of compares to insertion sort an array of the form 
-# { 0, 5, 1, 6, 2, 7, 3, 8, 4, 9 } is ~ 1/4 N^2.
-# ANSWER: False. The number of inversions is 0 + 1 + ... + N/2 - 1 ~ 1/8 N^2. 
-# Thus, the number of compares is ~ 1/8 N^2.
-# MY ANSWER: 19 compares on an actual run. 1/8 N^2=100/8=12.5 Anyone know why?
+# QUESTION: The number of compares to insertion sort an array of the form
+# { 0, 5, 1, 6, 2, 7, 3, 8, 4, 9 } is ~ 1/4 num_elems^2.
+# ANSWER: False. The number of inversions is 0 + 1 + ... + num_elems/2 - 1 ~ 1/8 num_elems^2.
+# Thus, the number of compares is ~ 1/8 num_elems^2.
+# MY ANSWER: 19 compares on an actual run. 1/8 num_elems^2=100/8=12.5 Anyone know why?
 #
-# QUESTION: The number of compares to insertion sort an array of N/2 1s 
-# interleaved with N/2 0s (e.g., 1 0 1 0 1 0 1 0 1 0) is ~ 1/8 N^2.
-# ANSWER: True. The number of inversions is 1 + 2 + 3 + ... + N/2 ~ 1/8 N^2. 
-# Thus, the number of compares is ~ 1/8 N^2.
-# MY ANSWER: 23 compares on an actual run. 1/8 N^2=100/8=12.5 Anyone know why?
+# QUESTION: The number of compares to insertion sort an array of num_elems/2 1s
+# interleaved with num_elems/2 0s (e.g., 1 0 1 0 1 0 1 0 1 0) is ~ 1/8 num_elems^2.
+# ANSWER: True. The number of inversions is 1 + 2 + 3 + ... + num_elems/2 ~ 1/8 num_elems^2.
+# Thus, the number of compares is ~ 1/8 num_elems^2.
+# MY ANSWER: 23 compares on an actual run. 1/8 num_elems^2=100/8=12.5 Anyone know why?
 
-# QUESTION: The number of compares to insertion sort an array of N/2 1s 
-# followed by N/2 0s (e.g., 1 1 1 1 1 0 0 0 0 0) is ~ 1/2 N^2.
-# ANSWER: False. The number of inversions is (N/2)*(N/2) = 1/4 N^2. 
-# Thus, the number of compares is ~ 1/4 N^2.
+# QUESTION: The number of compares to insertion sort an array of num_elems/2 1s
+# followed by num_elems/2 0s (e.g., 1 1 1 1 1 0 0 0 0 0) is ~ 1/2 num_elems^2.
+# ANSWER: False. The number of inversions is (num_elems/2)*(num_elems/2) = 1/4 num_elems^2.
+# Thus, the number of compares is ~ 1/4 num_elems^2.
 # MY ANSWER: 33 compares on an actual run.
 
-# QUESTION: The number of compares to insertion sort an array of N/2 keys 
-# in strictly increasing order followed by the same N/2 keys in strictly 
-# decreasing order (e.g., 0 1 2 3 4 4 3 2 1 0) is ~1/4 N^2.
-# ANSWER: The number of inversions is 0 + 2 + 4 + 6 + ... + (N-2) ~ 1/4 N^2. 
-# Thus, the number of compares is ~ 1/4 N^2.
+# QUESTION: The number of compares to insertion sort an array of num_elems/2 keys
+# in strictly increasing order followed by the same num_elems/2 keys in strictly
+# decreasing order (e.g., 0 1 2 3 4 4 3 2 1 0) is ~1/4 num_elems^2.
+# ANSWER: The number of inversions is 0 + 2 + 4 + 6 + ... + (num_elems-2) ~ 1/4 num_elems^2.
+# Thus, the number of compares is ~ 1/4 num_elems^2.
 # MY ANSWER: 29 compares on an actual run.
 
 
@@ -171,8 +178,8 @@ def Sort(ARR, array_history=None):
 #  # @param c the comparator specifying the order
 #  #/
 # def Sort(a, c):
-#     int N = len(a)
-#     for (int i = 0; i < N; i++):
+#     int num_elems = len(a)
+#     for (int i = 0; i < num_elems; i++):
 #         for (int j = i; j > 0 && less(c, a[j], a[j-1]); j--):
 #             _exch(a, j, j-1);
 #         assert _isSorted(a, c, 0, i)
@@ -185,18 +192,18 @@ def Sort(ARR, array_history=None):
  # @param a the array
  # @param array_history; Used in tests. When true prints ASCII Art demonstrating the sort
  # @return a permutation <tt>p[]</tt> such that <tt>a[p[0]]</tt>, <tt>a[p[1]]</tt>,
- #    ..., <tt>a[p[N-1]]</tt> are in ascending order
+ #    ..., <tt>a[p[num_elems-1]]</tt> are in ascending order
  #/
 
-def indexSort(ARR, array_history=None):
-  """Do not change ARR, return a new sorted version of ARR."""
-  N = len(ARR)
-  index = range(N)
-  for i in range(N):
-    j = i
-    while j > 0 and __lt__(ARR[index[j]], ARR[index[j-1]]):
-      _exch(index, j, j-1)
-      if array_history is not None: array_history.add_history(ARR, {j:'*', j-1:'*'})
-      j -= 1
-  return index
-
+def indexSort(arr, array_history=None):
+    """Do not change arr, return a new sorted version of arr."""
+    num_elems = len(arr)
+    index = range(num_elems)
+    for i in range(num_elems):
+        j = i
+        while j > 0 and __lt__(arr[index[j]], arr[index[j-1]]):
+            _exch(index, j, j-1)
+            if array_history is not None:
+                array_history.add_history(arr, {j:'*', j-1:'*'})
+            j -= 1
+    return index
--- tests/test_Insertion.py
+++ tests/test_Insertion.py
@@ -7,70 +7,68 @@ from AlgsSedgewickWayne.testcode.ArrayHistory import ArrayHistory
 from AlgsSedgewickWayne.testcode.InputArgs import cli_get_array
 
 def run(a, desc=None, prt=sys.stdout):
-  ah = ArrayHistory()
-  Sort(a, array_history=ah)
-  if desc is None:
-    desc = "INSERTION SORT" 
-  prt.write("{DESC} RESULT {A}\n".format(DESC=desc, A=' '.join(str(e) for e in a)))
-  ah.prt()
-  ah.show(desc)
+    ah = ArrayHistory()
+    Sort(a, array_history=ah)
+    if desc is None:
+        desc = "INSERTION SORT"
+    prt.write("{DESC} RESULT {A}\n".format(DESC=desc, A=' '.join(str(e) for e in a)))
+    ah.prt()
+    ah.show(desc)
 
 def test_wk2_lec():
-  run(map(int, "7 10 5 3 8 4 2 9 6".split()), "INSERTION SORT: SEED 183182")
+    run([int(e) for e in "7 10 5 3 8 4 2 9 6".split()], "INSERTION SORT: SEED 183182")
 
 def test_wk2_lec_best():
-  """Best-case runtime is when array is already sorted."""
-  run("A E E L M O P R S T X".split(), "INSERTION BEST SORT:")
+    """Best-case runtime is when array is already sorted."""
+    run("A E E L M O P R S T X".split(), "INSERTION BEST SORT:")
 
 def test_wk2_lec_worst():
-  """Best-case runtime is when array is already sorted."""
-  run("X T S R P O M L E E A".split(), "INSERTION WORST SORT:")
+    """Best-case runtime is when array is already sorted."""
+    run("X T S R P O M L E E A".split(), "INSERTION WORST SORT:")
 
 def test_wk2_lec_partial():
-  """Array is partially sorted ad contains inversions."""
-  # 6 inversions T-R  T-P  T-S  R-P  X-P  X-S
-  run("A E E L M O T R X P S".split(), "INSERTION PARTIAL SORT:")
+    """Array is partially sorted ad contains inversions."""
+    # 6 inversions T-R  T-P  T-S  R-P  X-P  X-S
+    run("A E E L M O T R X P S".split(), "INSERTION PARTIAL SORT:")
 
 def test_1():
-  run(map(int, "13 16 40 60 19 70 71 47 12 67".split()), "INSERTION SORT: SEED 183182")
+    run([int(e) for e in  "13 16 40 60 19 70 71 47 12 67".split()], "INSERTION SORT: SEED 183182")
 
 def test_2():
-  run("gold bone pink dust iris aqua rust kobi wine jade pine corn drab puce plum bark".split())
+    run("gold bone pink dust iris aqua rust kobi wine jade pine corn drab puce plum bark".split())
 
 def test_3():
-  run(map(int, "37 41 45 60 73 52 79 19 94 32".split()), "INSERTION SORT: seed 756506")
+    run([int(e) for e in  "37 41 45 60 73 52 79 19 94 32".split()], "INSERTION SORT: seed 756506")
 
 def test_4():
-  run(map(int, "14 23 77 94 96 48 39 18 41 13".split()), "INSERTION SORT: seed 72847")
+    run([int(e) for e in  "14 23 77 94 96 48 39 18 41 13".split()], "INSERTION SORT: seed 72847")
 
 def test_5():
-  run(map(int, "34 38 69 83 93 68 37 28 85 99".split()), "INSERTION SORT: seed 329024")
+    run([int(e) for e in  "34 38 69 83 93 68 37 28 85 99".split()], "INSERTION SORT: seed 329024")
 
 def test_half01a():
-  run(map(int, "0 1 0 1 0 1 0 1 0 1 0 1 0 1".split()), "INSERTION SORT: 01010101...")
+    run([int(e) for e in  "0 1 0 1 0 1 0 1 0 1 0 1 0 1".split()], "INSERTION SORT: 01010101...")
 
 def test_half01b():
-  run(map(int, "1 1 1 1 1 1 1 0 0 0 0 0 0 0".split()), "INSERTION SORT: 11110000")
+    run([int(e) for e in  "1 1 1 1 1 1 1 0 0 0 0 0 0 0".split()], "INSERTION SORT: 11110000")
 
 def test_half01c():
-  run(map(int, "0 0 0 0 0 0 0 1 1 1 1 1 1 1".split()), "INSERTION SORT: 00001111")
+    run([int(e) for e in  "0 0 0 0 0 0 0 1 1 1 1 1 1 1".split()], "INSERTION SORT: 00001111")
 
 def run_all():
-  """Run all tests."""
-  test_1()
-  test_2()
-  test_3()
-  test_4()
-  test_5()
+    """Run all tests."""
+    test_1()
+    test_2()
+    test_3()
+    test_4()
+    test_5()
 
 def cli():
-  N = len(sys.argv)
-  if N == 1:
-    run_all()
-  elif N == 2:
-    run(cli_get_array())
+    N = len(sys.argv)
+    if N == 1:
+        run_all()
+    elif N == 2:
+        run(cli_get_array())
 
 if __name__ == '__main__':
-  cli()
-
-
+    cli()
