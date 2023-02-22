63a57cf3c477a8e4fc9d745a3feb67318f22015b
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2023-02-22 16:45:01 -0500

    formatting on report

--- tests/test_regex_is_exponential.py
+++ tests/test_regex_is_exponential.py
@@ -19,28 +19,27 @@ def test_regex_is_exponential():
         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac",
     ]
 
-    # H MM SECONDS  Result Searching method                  Text on which to test for regex pattern
-    # - -- -------- ------ --------------------------------  ----------------------------------------
-    # 0:00:00.000436 False Python NFA("(a|aa)*b")            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 0:00:00.000374 False Python NFA("(a|aa)*b")            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 0:00:00.000388 False Python NFA("(a|aa)*b")            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 0:00:00.000489 False Python NFA("(a|aa)*b")            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 0:00:00.000469 False Python NFA("(a|aa)*b")            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 0:00:00.001158 False Python NFA("(a|aa)*b")            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 
+    # H MM SECONDS  Result Searching method              Text on which to test for regex pattern
+    # - -- -------- ------ ----------------------------- ----------------------------------------
+    # 0:00:00.000399 False Python NFA("(a|aa)*b")        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # 0:00:00.000374 False Python NFA("(a|aa)*b")        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # 0:00:00.000388 False Python NFA("(a|aa)*b")        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # 0:00:00.000489 False Python NFA("(a|aa)*b")        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # 0:00:00.000469 False Python NFA("(a|aa)*b")        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # 0:00:00.000423 False Python NFA("(a|aa)*b")        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
     regexp = "(a|aa)*b"
     _prt_hdr()
     for txt in txts:
         _run_nfa(regexp, txt)
 
-    # H MM SECONDS  Result Searching method                  Text on which to test for regex pattern
-    # - -- -------- ------ --------------------------------  ----------------------------------------
-    # 0:00:02.130942 False Python re(re.compile('(a|aa)*b')) aaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 0:00:05.454958 False Python re(re.compile('(a|aa)*b')) aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 0:00:15.768528 False Python re(re.compile('(a|aa)*b')) aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 0:00:38.932166 False Python re(re.compile('(a|aa)*b')) aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 0:01:45.282705 False Python re(re.compile('(a|aa)*b')) aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
-    # 0:05:02.419219 False Python re(re.compile('(a|aa)*b')) aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # H MM SECONDS  Result Searching method              Text on which to test for regex pattern
+    # - -- -------- ------ ----------------------------- ----------------------------------------
+    # 0:00:02.130942 False Python re.compile('(a|aa)*b') aaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # 0:00:05.454958 False Python re.compile('(a|aa)*b') aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # 0:00:15.768528 False Python re.compile('(a|aa)*b') aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # 0:00:38.932166 False Python re.compile('(a|aa)*b') aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # 0:01:45.282705 False Python re.compile('(a|aa)*b') aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
+    # 0:05:02.419219 False Python re.compile('(a|aa)*b') aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac
     regex = re.compile(r"(a|aa)*b")
     for txt in txts:
         _run_re(regex, txt)
@@ -50,7 +49,7 @@ def _run_nfa(regexp, txt):
     nfa = NFA(regexp)
     matched = nfa.recognizes(txt)
 
-    prt_hms(tic, f'{str(matched):5} Python NFA("{regexp}")            {txt}')
+    prt_hms(tic, f'{str(matched):5} Python NFA("{regexp}")        {txt}')
     return matched
 
 def _run_re(regexp, txt):
@@ -58,12 +57,12 @@ def _run_re(regexp, txt):
     tic = default_timer()
     result = regexp.search(txt)
     matched = result is not None
-    prt_hms(tic, f'{str(matched):5} Python re({regexp}) {txt}')
+    prt_hms(tic, f'{str(matched):5} Python {regexp} {txt}')
     return matched
 
 def _prt_hdr():
-    print('H MM SECONDS  Result Searching method                  Text on which to test for regex pattern')
-    print('- -- -------- ------ --------------------------------  ----------------------------------------')
+    print('H MM SECONDS  Result Searching method              Text on which to test for regex pattern')
+    print('- -- -------- ------ ----------------------------  ----------------------------------------')
 
 
 #****************************************************************************
